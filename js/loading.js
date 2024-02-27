b6ab2057ac8a385f32e60a1f813788aadcd3223f
Cecilia Urbieta <142773954+ceciliaU1@users.noreply.github.com>
2024-02-27 22:44:05 +0000

    Tue, Feb 27, 2024, 2:44 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,14 @@
 loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
-loadText.innerText = "5%"
-console.log("HELLO")
+load = 0
+
+
+
+
+function blurring() {
+    load++
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}

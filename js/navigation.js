1083ded4ac534371659323763f5c29e474985ee9
Cecilia Urbieta <142773954+ceciliaU1@users.noreply.github.com>
2024-03-08 22:57:11 +0000

    Fri, Mar 8, 2024, 2:57 PM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -3,9 +3,10 @@ nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if (window.scrollY > 223) {
-        nav.calssList.add('active')
-    } else {
-        nav.classList.remove('active')
-    }
+    console.log("hello")
+    // if (window.scrollY > 223) {
+    //     nav.calssList.add('active')
+    // } else {
+    //     nav.classList.remove('active')
+    // }
 }
